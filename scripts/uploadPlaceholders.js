d5873c187b4ac3e8618ff457f77effc4e872d3d4
Brian Vallelunga <vallelungabrian@gmail.com>
2015-10-07 14:24:33 -0700

    updated placehold js

--- scripts/uploadPlaceholders.js
+++ scripts/uploadPlaceholders.js
@@ -2,11 +2,16 @@ var fs = require("fs")
 var Parse = require("parse/node")
 var VoterImage = Parse.Object.extend("Image")
 var easyimg = require('easyimage')
-var path = process.argv[2]
-var gender = process.argv[3]
+var mode = process.argv[2]
+var path = process.argv[3]
+var gender = process.argv[4]
 var tmp = "/tmp/Placeholders" + Math.ceil(Math.random() * 500)
 
-Parse.initialize("q1NZZSGYNxaYIQq5dDNkMlD407fmm2Hq6BoXBzu4", "m736Jb7Z8atZGPSfW7eBnrOKwJNyDDSUFmTOVT5G")
+if(mode == "release") {
+	Parse.initialize("2umHhGkMh7Gm6dU8rRVgHSPzbW5xOymXzenDx2XC", "hu5nnIlQYKc22ly6JAZE3LxcNTPVYGHdhYAyDYr3")
+} else {
+	Parse.initialize("q1NZZSGYNxaYIQq5dDNkMlD407fmm2Hq6BoXBzu4", "m736Jb7Z8atZGPSfW7eBnrOKwJNyDDSUFmTOVT5G")
+}
 
 if(!fs.existsSync(tmp)) {
   fs.mkdirSync(tmp)
