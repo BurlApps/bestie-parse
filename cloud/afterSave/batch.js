f5a0a074a56d7857b8a90d0442e02ca23411675f
Brian Vallelunga <vallelungabrian@gmail.com>
2015-10-26 19:15:24 -0700

    fixed typo with notifications

--- cloud/afterSave/batch.js
+++ cloud/afterSave/batch.js
@@ -3,7 +3,7 @@ Parse.Cloud.afterSave("Batch", function(req, res) {
   var active = object.get("active")
   var votes = object.get("votes")
   var userVotes = object.get("userVotes")
-  var maxVotes = object.get("userVotes")
+  var maxVotes = object.get("maxVotes")
   var alerted = object.get("alerted")
   var override = object.get("override")
   
